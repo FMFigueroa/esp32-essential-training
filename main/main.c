36349b05d120721d3fef30edb0ac7d0c1f982ce3
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-08 02:31:27 -0300

    feat:✨ Delays

--- main/main.c
+++ main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define TAG "DELAY"
 
 void app_main(void)
 {
-    printf("Hello World\n");
+    int i = 0;
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "in loop %d", i++);
+    }
 }
