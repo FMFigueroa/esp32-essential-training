d3497bac864ac4e2e881382537b387158bcbd5fc
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-08 13:28:14 -0300

    feat:✨ Random Number

--- main/main.c
+++ main/main.c
@@ -2,15 +2,27 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+// #include "esp_system.h" //For IDF 4.x use this lib
+#include "esp_random.h" //esp_system.h" For IDF 5.x use this lib
+#include "math.h"
 
-#define TAG "DELAY"
+#define TAG "DICE"
+
+int dice_role()
+{
+    int random = esp_random();
+    int positiveNumber = abs(random);          // the function abs() convert from number random to positive number random.!
+    int diceNumber = (positiveNumber % 6) + 1; // 0 - 5 = 0... > 6 = 1 ;
+    return diceNumber;
+}
 
 void app_main(void)
 {
-    int i = 0;
     while (1)
     {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "in loop %d", i++);
+        // ESP_LOGI(TAG, "random number %d", esp_random()); // For IDF 4.x use this sintaxis
+        // ESP_LOGI(TAG, "random number %ld", esp_random());
+        ESP_LOGI(TAG, "random number %d", dice_role());
     }
 }
