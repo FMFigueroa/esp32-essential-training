d6a851a92e417c5f9eb3af62942ec21399236d90
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-08 17:53:37 -0300

    feat:✨ Led tBlinking

--- main/main.c
+++ main/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define PIN 2
 
 void app_main(void)
 {
-    printf("Hello World\n");
+    // GPIO mode setting
+    gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+    uint32_t isOn = 0;
+    while (true)
+    {
+        isOn = !isOn;
+        // GPIO toggle setting
+        gpio_set_level(PIN, isOn);
+        // Delay
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
