5b0a0278160d60446bd19afdd82283c45fb1a646
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-12 14:49:18 -0300

    feat:✨ Timer vs Tasks

--- main/main.c
+++ main/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <esp_timer.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
 
 void app_main(void)
 {
-    printf("Hello World\n");
+    //portTICK_PERIOD_MS
+    printf("\n");
+    printf("Timer miliseconds 100:\n");
+    printf("Timer: %lld milisecond\n", esp_timer_get_time() / 1000);
+    vTaskDelay(100 / portTICK_PERIOD_MS); // 1 Tick o 10ms
+    printf("Timer: %lld milisecond\n", esp_timer_get_time() / 1000);
+
+    // pdMS_TO_TICKS
+    printf("\n");
+    printf("Timer miliseconds 100:\n");
+    printf("Timer: %lld milisecond\n", esp_timer_get_time() / 1000);
+    vTaskDelay(pdMS_TO_TICKS(100));
+    printf("Timer: %lld milisecond\n", esp_timer_get_time() / 1000);
 }
