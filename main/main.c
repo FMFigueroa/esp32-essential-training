4000b4f9e94e334baaa620dcd6962a9a3e2c0017
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-12 18:29:46 -0300

    feat:✨ Using the Second Core

--- main/main.c
+++ main/main.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main(void)
+void task1(void *params) // char * params
 {
-    printf("Hello World\n");
+
+    while (true)
+    {
+        printf("reading temperature from %s\n", (char *)params);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void task2(void *params) // char * params
+{
+    while (true)
+    {
+        printf("reading humidity from %s\n", (char *)params);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main()
+{
+    xTaskCreate(&task1, "temperature reading", 2048, "task 1", 2, NULL);
+    xTaskCreatePinnedToCore(&task2, "humidity reading", 2048, "task 2", 2, NULL,1);
 }
