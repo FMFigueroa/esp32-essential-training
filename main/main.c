7084d713e0ecec05f714d307954329801d51d5d5
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-12 16:27:34 -0300

    feat:✨ FreeRTOS Create  Simple Tasks

--- main/main.c
+++ main/main.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main(void)
+void task1(void *params) // char * params
 {
-    printf("Hello World\n");
+
+    while (true)
+    {
+        printf("reading temperature from %s\n", (char *)params);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void task2(void *params) // int * params
+{
+    while (true)
+    {
+        printf("reading humidity from %s\n", (char *)params);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main()
+{
+    xTaskCreate(&task1, "temperature reading", 2048, "task 1", 2, NULL);
+    xTaskCreate(&task2, "humidity reading", 2048, "task 2", 2, NULL);
 }
