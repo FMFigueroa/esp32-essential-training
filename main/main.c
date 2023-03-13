7da0866e42b088fed316df4c937033ec804950fa
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-13 19:02:20 -0300

    Different Notifications with Parameter of a Task

--- main/main.c
+++ main/main.c
@@ -2,37 +2,35 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-static TaskHandle_t myTask1Handle = NULL;
-static TaskHandle_t myTask2Handle = NULL;
+static TaskHandle_t myTask1Handler = NULL;
 
 void sender(void *params) // char * params
 {
     while (1)
     {
-        /* dispatcher receive as param to where will send notification,
-        can to be a task or several task the same time. */
-        xTaskNotifyGive(myTask2Handle);
-        xTaskNotifyGive(myTask2Handle);
-        xTaskNotifyGive(myTask2Handle);
-        xTaskNotifyGive(myTask2Handle);
-        vTaskDelay(5000 / portTICK_PERIOD_MS); //send notification each 5 milisegundo
+
+        xTaskNotify(myTask1Handler, (1 << 2), eSetValueWithoutOverwrite); //0001 << 2= 0100 = 4 (Shift Left)
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        xTaskNotify(myTask1Handler, (1 | 4 ), eSetValueWithoutOverwrite); //0001 | 0100 = 0101 = 5 (OR)
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        xTaskNotify(myTask1Handler, (12 >> 1), eSetValueWithoutOverwrite); //1100 >> 1= 0110 = 6 (Shift Right) 
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        xTaskNotify(myTask1Handler, (15 & 7), eSetValueWithoutOverwrite); // 1111 & 0111 = 7 (AND)
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
-
 void receiver(void *params) // char * params
 {
-    int notificationValue;
+    uint state; // uint32_t * pulNotificationValue
     while (1)
     {
-        notificationValue = ulTaskNotifyTake(pdFALSE, (TickType_t)portMAX_DELAY);
-        if (notificationValue > 0) {
-                printf("recived notification: %d\r\n", notificationValue);
-        }    
+        xTaskNotifyWait(0, 0, &state, portMAX_DELAY);
+        printf("received state: %d  times\n", state);
     }
 }
 
 void app_main()
 {
-    xTaskCreate(&receiver, "sender", 2048, "NULL", 2, &myTask2Handle);
+    xTaskCreate(&receiver, "sender", 2048, "NULL", 2, &myTask1Handler);
     xTaskCreate(&sender, "receiver", 2048, "NULL", 2, NULL);
 }
