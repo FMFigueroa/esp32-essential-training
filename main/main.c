12e772392c440b71c7fb0bd8de1bfbc7b6589fc5
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-03-13 22:12:04 -0300

    feat:✨ Mutex

--- main/main.c
+++ main/main.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+SemaphoreHandle_t mutexBus;
+// xSemaphoreHandle mutexBus;
+
+void writeToBus(char *message)
+{
+    printf(message);
+}
+
+void task1(void *params)
+{
+    while (true)
+    {
+        printf("reading temperature \n");
+        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
+        {
+            writeToBus("tempratrue is 25c\n");
+            xSemaphoreGive(mutexBus);
+        }
+        else
+        {
+            printf("writing temperature timed out \n");
+        }
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+
+void task2(void *params)
+{
+    while (true)
+    {
+        printf("reading humidity\n");
+        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
+        {
+            writeToBus("humidity is 50 \n");
+            xSemaphoreGive(mutexBus);
+        }
+        else
+        {
+            printf("writing humidity timed out \n");
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void)
 {
-    printf("Hello World\n");
+    mutexBus = xSemaphoreCreateMutex();
+    xTaskCreate(&task1, "temperature reading", 2048, NULL, 2, NULL);
+    xTaskCreate(&task2, "humidity reading", 2048, NULL, 2, NULL);
 }
