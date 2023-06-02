4e0c0188ec8c28815c06c9459ec8d17f2a1d78dc
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-02 14:41:27 -0300

    feat:✨ WiFi Connect

--- main/main.c
+++ main/main.c
@@ -1,6 +1,50 @@
+#include "sdkconfig.h"
 #include <stdio.h>
+#include "connect.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+static const char *TAG = "wifi_connect";
+
+void initialize_nvs(void)
+{
+    // initialize the default NVS partition
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
+
+void wifi_connect(void *params)
+{
+    wifi_connect_ap("my-esp-ssid", "password");
+
+    esp_err_t err = wifi_connect_sta(CONFIG_SSID, CONFIG_PASSWORD, 10000);
+    if (err)
+    {
+        ESP_LOGE(TAG, "Failed to connect");
+        vTaskDelete(NULL);
+    }
+
+    // Disconnect Wifi
+    /*     for (size_t i = 5; i > 0; i--)
+        {
+            printf("disconnecting in %d\n", i);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+        }
+        wifi_disconnect();
+    */
+    vTaskDelete(NULL);
+}
 
 void app_main(void)
 {
-    printf("Hello World\n");
+    initialize_nvs();
+    wifi_init();
+    xTaskCreate(wifi_connect, "wifi_connect", 1024 * 5, NULL, 5, NULL);
 }
